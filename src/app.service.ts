9675112133d7ee929d386eb87089315d64d48c9f
Eslam Gomaa <148918144+eslamgh1@users.noreply.github.com>
2025-12-29 18:54:58 +0200

    notes are added

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from main App service!';
+    return 'New Project: High-Performance E-commerce Backend with NestJS & TypeScript- Deployment: Successfully deployed the application on Railway';
   }
 }
